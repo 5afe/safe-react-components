32bbc139f488ade65a505d21f2009f484ae3748a
Agustín Longoni <agustin.longoni@altoros.com>
2020-03-25 10:55:10 -0300

    Add Layout

---
+++ src/layout/layout.stories.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Layout from "./index";
+
+export default {
+  title: "Layout",
+  component: Layout,
+  parameters: {
+    componentSubtitle: "Basic Layout"
+  }
+};
+
+export const layout = () => <Layout />;
