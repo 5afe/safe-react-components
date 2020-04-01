2b3425063ae13a45194d7961cbf96f0564be76e6
nicosampler <nf.dominguez.87@gmail.com>
2020-04-01 12:44:28 -0300

    comment surfaces from index

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ export * from './inputs';
 export * from './feedback';
 export * from './navigation';
 //export * from './layouts'
-export * from './surfaces';
+//export * from './surfaces';
 //export * from './utils'
 export { default as theme } from './theme';
