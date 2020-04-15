eb0ac7d38a3b5dc867f194b14aa7b2dafa564335
nicosampler <nf.dominguez.87@gmail.com>
2020-04-15 13:45:34 -0300

    export Card

--- src/dataDisplay/index.ts
+++ src/dataDisplay/index.ts
@@ -1,7 +1,8 @@
-export { default as Icon } from './Icon'
-export { default as FixedIcon } from './FixedIcon'
-export { default as Text } from './Text'
-export { default as Title } from './Title'
-export { default as Section } from './Section'
-export { default as Divider } from './Divider'
-export { default as Layout } from './Layout'
+export { default as Card } from './Card';
+export { default as Divider } from './Divider';
+export { default as FixedIcon } from './FixedIcon';
+export { default as Icon } from './Icon';
+export { default as Layout } from './Layout';
+export { default as Text } from './Text';
+export { default as Title } from './Title';
+export { default as Section } from './Section';
