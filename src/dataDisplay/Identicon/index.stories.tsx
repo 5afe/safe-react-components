ca3b021928af9b8d8028f2f8153dcb38ffddf189
Agustin Pane <agustin.pane@gmail.com>
2020-06-10 19:42:12 -0300

    Adds identicon component Adds ethereum-blockies-base64

---
+++ src/dataDisplay/Identicon/index.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Identicon from './index';
+
+export default {
+  title: 'Data Display/Identicon',
+  component: Identicon,
+  parameters: {
+    componentSubtitle: 'Identicon Component.'
+  }
+};
+
+export const sizes = () => {
+  return (
+    <>
+      <Identicon address={"thisisanexample"} diameter={32}/>
+    </>
+  );
+};
---
+++ src/dataDisplay/Identicon/index.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+
+import makeBlockie from 'ethereum-blockies-base64';
+import { RefObject, useEffect, useState } from 'react';
+
+type Props = {
+  address: string;
+  diameter: number;
+};
+
+type StyleProps = {
+  width: number;
+  height: number;
+};
+
+const getStyleFrom = (diameter: number): StyleProps => ({
+  width: diameter,
+  height: diameter
+});
+
+const generateBlockieIdenticon = (
+  address: string,
+  diameter: number
+): HTMLImageElement => {
+  const image = new window.Image();
+  image.src = makeBlockie(address);
+  image.height = diameter;
+  image.width = diameter;
+  image.style.borderRadius = `${diameter / 2}px`;
+
+  return image;
+};
+
+const Identicon = (props: Props) => {
+  const { address, diameter } = props;
+  const style: StyleProps = getStyleFrom(diameter);
+
+  const [identicon, setIdenticon] = useState<RefObject<HTMLImageElement> | null>(null);
+
+  useEffect(() => {
+    setIdenticon(React.createRef<HTMLImageElement>());
+  }, []);
+
+  useEffect(() => {
+    const image = generateBlockieIdenticon(address, diameter);
+
+    if (!identicon || !identicon.current) {
+      return;
+    }
+
+    const { children } = identicon.current;
+    for (let i = 0; i < children.length; i += 1) {
+      identicon.current.removeChild(children[i]);
+    }
+
+    identicon.current.appendChild(image);
+  }, [address, diameter, identicon]);
+
+  return <div ref={identicon} style={style} />;
+};
+
+export default Identicon;
