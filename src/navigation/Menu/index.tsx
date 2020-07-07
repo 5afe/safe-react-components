310e3281797f125ccfb3e6612ca8576fb0eb8e62
nicolas <nicosampler@users.noreply.github.com>
2020-07-07 12:14:17 -0300

    Fix: Add className to IconText and Menu (#28)
    * Fix: Add className to IconText
    
    * add className to Menu
    
    * review changes

--- src/navigation/Menu/index.tsx
+++ src/navigation/Menu/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  margin: 8px 0;
   padding: 16px 0;
   box-sizing: border-box;
   max-height: 54px;
@@ -13,9 +12,12 @@ const Wrapper = styled.div`
 `;
 
 type Props = {
-  children: any;
+  children: React.ReactNode;
+  className?: string;
 };
 
-const Menu = ({ children }: Props) => <Wrapper>{children}</Wrapper>;
+const Menu = ({ children, className }: Props): React.ReactElement => (
+  <Wrapper className={className}>{children}</Wrapper>
+);
 
 export default Menu;
