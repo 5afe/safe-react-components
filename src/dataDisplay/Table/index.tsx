d2d598ec9f982ac0db398faaa9d2e5ccc152aee0
nicosampler <nf.dominguez.87@gmail.com>
2020-07-13 16:16:28 -0300

    refactor Table

--- src/dataDisplay/Table/index.tsx
+++ src/dataDisplay/Table/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
+//import { makeStyles } from '@material-ui/core/styles';
+import TableMui from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
@@ -10,122 +11,115 @@ import Paper from '@material-ui/core/Paper';
 // import Box from '@material-ui/core/Box';
 // import Collapse from '@material-ui/core/Collapse';
 // import IconButton from '@material-ui/core/IconButton';
-import styled from 'styled-components';
+//import styled from 'styled-components';
 
-import theme from '../../theme'; 
-import Icon from '../Icon';
-import Text from '../Text';
+// import theme from '../../theme';
+//import Icon from '../Icon';
+//import Text from '../Text';
 
-const useStyles = makeStyles({
-  table: {
-  //  minWidth: 450
+// const useStyles = makeStyles({
+//   table: {
+//     //  minWidth: 450
+//     // '& .MuiTableRow-root:hover': {
+//     //   backgroundColor: theme.colors.background,
+//     //   cursor: 'pointer'
+//     // },
+//   }
+// });
 
-  '& .MuiTableRow-root:hover': {
-    backgroundColor: theme.colors.background,
-    cursor: 'pointer'
-  },
-  }
-});
+// const TextTHead = styled(Text)`
+//   text-transform: uppercase;
+// `;
 
-const TextTHead = styled(Text)`
-  text-transform: uppercase;
-`;
+// const StyledTableCell = styled(TableCell)`
+//   border-bottom: 2px solid ${({ theme }) => theme.colors.separator} !important;
+// `;
 
-const StyledTableCell = styled(TableCell)`
-  border-bottom: 2px solid ${({ theme }) => theme.colors.separator} !important;
-`;
-
-function createData(
-  name: string,
-  spent: number,
-  reset: number,
-  other1: number,
-  other: number
-) {
-  return {
-    name,
-    spent,
-    reset,
-    other,
-    other1: (
-      <Text color="text" size="md">
-        {other1}
-      </Text>
-    ),
-    other2: (
-      <>
-        <Icon size="sm" type="edit" color="icon" />
-        <Icon size="sm" type="delete" color="icon" />
-      </>
-    )
-  };
+export enum Alignment {
+  left = 'left',
+  right = 'right'
 }
 
-const rows = [
-  createData('Owner #1', 159, 6.0, 24, 4.0),
-  createData('Owner #2', 237, 9.0, 37, 4.3),
-  createData('Owner #3', 262, 16.0, 24, 6.0),
-  createData('Owner #4', 305, 3.7, 67, 4.3),
-  createData('Owner #5', 356, 16.0, 49, 3.9)
-];
+export enum SortDirection {
+  asc = 'asc',
+  desc = 'desc'
+}
 
-export default function SimpleTable() {
-  const classes = useStyles();
+export type Header = {
+  id: string;
+  alignment?: Alignment;
+  label: string;
+};
+
+export type Row = {
+  id: string;
+  cells: Array<{
+    id?: string;
+    alignment?: Alignment;
+    content: React.ReactNode;
+  }>;
+};
+
+type Props = {
+  headers?: Header[];
+  rows: Row[];
+  className?: string;
+  selectedRowIds?: Set<string>;
+  sortedByHeaderId?: string;
+  sortDirection?: SortDirection;
+  onHeaderClick?: (id: string) => void;
+  onRowClick?: (id: string) => void;
+};
+
+const Table = ({
+  headers,
+  rows,
+  className,
+  selectedRowIds,
+  sortedByHeaderId,
+  sortDirection,
+  onHeaderClick
+}: Props) => {
+  //const classes = useStyles();
 
   return (
     <TableContainer component={Paper} elevation={3}>
-      <Table className={classes.table} aria-label="simple table">
+      <TableMui className={className}>
+        {/* HEADER CELLS */}
         <TableHead>
           <TableRow>
-            <StyledTableCell>
-              <TextTHead size="sm" color="secondaryLight" strong>
-                Beneficiary
-              </TextTHead>
-            </StyledTableCell>
-            <StyledTableCell>
-              <TextTHead size="sm" color="secondaryLight" strong>
-                Spent
-              </TextTHead>
-            </StyledTableCell>
-            <StyledTableCell>
-              <TextTHead size="sm" color="secondaryLight" strong>
-                Reset time
-              </TextTHead>
-            </StyledTableCell>
-            <StyledTableCell>
-              <TextTHead size="sm" color="secondaryLight" strong>
-                Other
-              </TextTHead>
-            </StyledTableCell>
-            <StyledTableCell>
-              <TextTHead size="sm" color="secondaryLight" strong>
-                Actions
-              </TextTHead>
-            </StyledTableCell>
+            {headers?.map((header) => (
+              <TableCell
+                key={header.id}
+                align={header.alignment || Alignment.left}>
+                <TableSortLabel
+                  active={sortedByHeaderId === header.id}
+                  direction={sortDirection}
+                  onClick={() => onHeaderClick && onHeaderClick(header.id)}>
+                  {header.label}
+                </TableSortLabel>
+              </TableCell>
+            ))}
           </TableRow>
         </TableHead>
+
+        {/* TABLE BODY */}
         <TableBody>
           {rows.map((row) => (
-            <TableRow key={row.name}>
-              <StyledTableCell component="th" scope="row">
-                <Text color="text" size="md">{row.name}</Text>
-              </StyledTableCell>
-              <StyledTableCell>
-                <Text color="text" size="md">{row.spent}</Text>
-              </StyledTableCell>
-              <StyledTableCell>
-                <Text size="md">{row.reset}</Text>
-              </StyledTableCell>
-              <StyledTableCell>
-              {row.other1}
-              </StyledTableCell>
-              <StyledTableCell>
-              {row.other2}
-              </StyledTableCell>
+            <TableRow hover key={row.id} selected={selectedRowIds?.has(row.id)}>
+              {row.cells.map((c, index) => (
+                <TableCell
+                  key={c.id || index}
+                  align={c.alignment || Alignment.left}>
+                  {c.content}
+                </TableCell>
+              ))}
             </TableRow>
           ))}
         </TableBody>
-      </Table>
+      </TableMui>
     </TableContainer>
   );
-}
+};
+
+export default Table;
--- src/dataDisplay/Table/table.stories.tsx
+++ src/dataDisplay/Table/table.stories.tsx
@@ -1,17 +1,117 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import Table from './index';
+import Table, { Alignment, SortDirection } from './index';
+import { Icon } from '../../';
 
 export default {
   title: 'Data Display/Table',
   component: Table,
   parameters: {
-    componentSubtitle: 'Used to tabular information.'
+    componentSubtitle:
+      'Used for tabular information. It allows sorting by a single column.'
   }
 };
 
-export const section = () => (
-  <>
-    <Table />
-  </>
-);
+export const section = () => {
+  const [selectedRowIds, setSelectedRowIds] = useState<Set<string>>(new Set());
+  const [sortedByHeaderId, setSortedByHeaderId] = useState<string | undefined>(
+    'col2'
+  );
+  const [sortDirection, setSortDirection] = useState<SortDirection>(
+    SortDirection.asc
+  );
+
+  const onRowClick = (rowId: string) => {
+    const cp = new Set(selectedRowIds);
+    if (cp.has(rowId)) {
+      cp.delete(rowId);
+    } else {
+      cp.add(rowId);
+    }
+    setSelectedRowIds(cp);
+  };
+
+  const onHeaderClick = (headerId: string) => {
+    if (!['col2'].includes(headerId)) {
+      return;
+    }
+
+    let newDirection =
+      sortedByHeaderId === headerId && sortDirection === SortDirection.asc
+        ? SortDirection.desc
+        : SortDirection.asc;
+
+    setSortDirection(newDirection);
+    setSortedByHeaderId(headerId);
+  };
+
+  const getSortedRows = () => {
+    const cp = [...rows];
+    return cp.sort((a, b) => {
+      return sortDirection === SortDirection.asc
+        ? Number(a.cells[1].content) - Number(b.cells[1].content)
+        : Number(b.cells[1].content) - Number(a.cells[1].content);
+    });
+  };
+
+  const headerCells = [
+    {
+      id: 'col1',
+      label: 'col1'
+    },
+    {
+      id: 'col2',
+      alignment: Alignment.right,
+      label: 'col2'
+    },
+    {
+      id: 'col3',
+      label: 'col3'
+    }
+  ];
+
+  const rows = [
+    {
+      id: '1',
+      cells: [
+        {
+          content: <Icon type="addressBook" size="sm" />
+        },
+        {
+          content: 1,
+          alignment: Alignment.right
+        },
+        {
+          content: 'safe'
+        }
+      ]
+    },
+    {
+      id: '2',
+      cells: [
+        {
+          content: <Icon type="apps" size="sm" />
+        },
+        {
+          content: 2,
+          alignment: Alignment.right
+        },
+        {
+          content: 'gnosis'
+        }
+      ]
+    }
+  ];
+
+  return (
+    <Table
+      headers={headerCells}
+      rows={getSortedRows()}
+      selectedRowIds={selectedRowIds}
+      sortedByHeaderId={sortedByHeaderId}
+      sortDirection={sortDirection}
+      onRowClick={onRowClick}
+      onHeaderClick={onHeaderClick}
+    />
+  );
+};
