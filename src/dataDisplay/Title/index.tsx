f2b47c9e6bac10e04f170e4cd65e11a1623af537
Agustín Longoni <agustin.longoni@altoros.com>
2020-09-30 09:22:56 -0300

    add strong/bold property fot Tiltle component

--- src/dataDisplay/Title/index.tsx
+++ src/dataDisplay/Title/index.tsx
@@ -7,45 +7,46 @@ type Props = {
   children: string | React.ReactNode;
   size: ThemeTitleSize;
   withoutMargin?: boolean;
+  strong?: boolean;
 };
 
-const StyledH1 = styled.h1<{ withoutMargin?: boolean }>`
+const StyledH1 = styled.h1<{ withoutMargin?: boolean; strong?: boolean }>`
   font-family: 'Averta';
   font-size: ${({ theme }) => theme.title.size.xl.fontSize};
   line-height: ${({ theme }) => theme.title.size.xl.lineHeight};
-  font-weight: normal;
+  font-weight: ${({ strong }) => (strong ? 'bold' : 'normal')};
   margin: ${({ withoutMargin }) => (withoutMargin ? 0 : '30px')} 0;
 `;
 
-const StyledH2 = styled.h2<{ withoutMargin?: boolean }>`
+const StyledH2 = styled.h2<{ withoutMargin?: boolean; strong?: boolean }>`
   font-family: 'Averta';
   font-size: ${({ theme }) => theme.title.size.lg.fontSize};
   line-height: ${({ theme }) => theme.title.size.lg.lineHeight};
-  font-weight: normal;
+  font-weight: ${({ strong }) => (strong ? 'bold' : 'normal')};
   margin: ${({ withoutMargin }) => (withoutMargin ? 0 : '28px')} 0;
 `;
 
-const StyledH3 = styled.h3<{ withoutMargin?: boolean }>`
+const StyledH3 = styled.h3<{ withoutMargin?: boolean; strong?: boolean }>`
   font-family: 'Averta';
   font-size: ${({ theme }) => theme.title.size.md.fontSize};
   line-height: ${({ theme }) => theme.title.size.md.lineHeight};
-  font-weight: normal;
+  font-weight: ${({ strong }) => (strong ? 'bold' : 'normal')};
   margin: ${({ withoutMargin }) => (withoutMargin ? 0 : '26px')} 0;
 `;
 
-const StyledH4 = styled.h4<{ withoutMargin?: boolean }>`
+const StyledH4 = styled.h4<{ withoutMargin?: boolean; strong?: boolean }>`
   font-family: 'Averta';
   font-size: ${({ theme }) => theme.title.size.sm.fontSize};
   line-height: ${({ theme }) => theme.title.size.sm.lineHeight};
-  font-weight: normal;
+  font-weight: ${({ strong }) => (strong ? 'bold' : 'normal')};
   margin: ${({ withoutMargin }) => (withoutMargin ? 0 : '22px')} 0;
 `;
 
-const StyledH5 = styled.h5<{ withoutMargin?: boolean }>`
+const StyledH5 = styled.h5<{ withoutMargin?: boolean; strong?: boolean }>`
   font-family: 'Averta';
   font-size: ${({ theme }) => theme.title.size.xs.fontSize};
   line-height: ${({ theme }) => theme.title.size.xs.lineHeight};
-  font-weight: normal;
+  font-weight: ${({ strong }) => (strong ? 'bold' : 'normal')};
   margin: ${({ withoutMargin }) => (withoutMargin ? 0 : '18px')} 0;
 `;
 
