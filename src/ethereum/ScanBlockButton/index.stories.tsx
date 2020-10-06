15210376e2b1dcbb77405dea55104309532467c0
Agustin Pane <agustin.pane@gmail.com>
2020-10-06 12:07:47 -0300

    Replaces example 2 with blockscout on scanBlockButton

--- src/ethereum/ScanBlockButton/index.stories.tsx
+++ src/ethereum/ScanBlockButton/index.stories.tsx
@@ -15,8 +15,7 @@ const StyledText = styled(Text)`
 `;
 
 const hash = '0xda6786379ff88729264d31d472fa917f5e561443';
-const hash2 =
-  '0xc276be3ffccc2398d3b82a0e375a94f67b8ad81c68f8625a5d516567dfe3de29';
+const hash2 = '0x72d9E579f691D62aA7e0703840db6dd2fa9fAE21';
 
 const explorerUrl1 = () => ({
   alt: `Show details on Etherscan`,
@@ -24,8 +23,8 @@ const explorerUrl1 = () => ({
 });
 
 const explorerUrl2 = () => ({
-  alt: `Show details on Etherscan`,
-  url: `https://etherscan.io/address/${hash2}`,
+  alt: `Show details on BlockScout`,
+  url: `https://blockscout.com/poa/xdai/address/${hash2}`,
 });
 
 export const SimpleScanBlockButton = (): React.ReactElement => (
