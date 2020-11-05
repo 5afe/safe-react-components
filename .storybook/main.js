9b73ad325fc54a5a675c3d14cc25ebe9413cbed6
Daniel Sanchez <daniel.sanchez@gnosis.pm>
2020-11-05 11:42:27 +0100

    Upgrade typescript to v4 and remove create-react-app storybook dependency

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   stories: ["../src/**/*.stories.tsx"],
   addons: [
-    "@storybook/preset-create-react-app",
     "@storybook/addon-actions",
     "@storybook/addon-links",
     "@storybook/addon-docs",
