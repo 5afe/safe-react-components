9f8bcf6599a0db10b15a769510ed86d8c49426f7
nicosampler <nf.dominguez.87@gmail.com>
2020-11-27 13:24:38 -0300

    remove font

--- src/dataDisplay/Icon/icon.stories.tsx
+++ src/dataDisplay/Icon/icon.stories.tsx
@@ -28,7 +28,7 @@ export const Icons = (): React.ReactElement => {
     width: 140px;
     height: 140px;
     border: 1px solid ${({ theme }) => theme.colors.background};
-    font-family: ${({ theme }) => theme.fonts.fontFamily}, sans-serif;
+    font-family: ${({ theme }) => theme.fonts.fontFamily};
     font-size: 14px;
   `;
 
