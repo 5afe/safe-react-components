b6a6cf03f0b11ba9af979ba0325222eb46fa1d16
Agustín Longoni <agustin.longoni@altoros.com>
2020-12-02 10:24:00 -0300

    add disabled prop to Card

--- src/dataDisplay/Card/index.tsx
+++ src/dataDisplay/Card/index.tsx
@@ -4,22 +4,38 @@ import { rgba } from 'polished';
 
 const StyledCard = styled.div`
   box-shadow: 1px 2px 10px 0
-    ${({ theme }) => rgba(theme.colors.shadow.color, 0.08)};
+    ${({ theme }) => rgba(theme.colors.shadow.color, 0.18)};
   border-radius: 8px;
   padding: 24px;
   background-color: ${({ theme }) => theme.colors.white};
+  z-index: 1;
+`;
+
+const Disabled = styled.div`
+  opacity: 0.5;
+  pointer-events: none;
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.white};
+  z-index: 999;
+  top: 0;
+  left: 0;
 `;
 
 type Props = {
   className?: string;
+  isDisabled?: boolean;
 } & React.HTMLAttributes<HTMLDivElement>;
 
 const Card: React.FC<Props> = ({
   className,
   children,
+  isDisabled,
   ...rest
 }): React.ReactElement => (
   <StyledCard className={className} {...rest}>
+    {isDisabled && <Disabled />}
     {children}
   </StyledCard>
 );
