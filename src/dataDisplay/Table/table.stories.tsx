ebc5ad3b1fd1a5c9f30d4799257458d55b02665f
Manuel Gellfart <manuel.gellfart@gmail.com>
2021-09-06 17:22:22 +0200

    Adds support for fixed table header (#148)
    - updates storybook
    - adds isStickyHeader property
    - adds maxHeight property
    
    When both are set and the table contents exceed the maxHeight,
    the table is scrollable with a fixed header
    
    Co-authored-by: schmanu <schmanu@users.noreply.github.com>

--- src/dataDisplay/Table/table.stories.tsx
+++ src/dataDisplay/Table/table.stories.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { Table, TableAlignment, TableSortDirection } from './index';
+import { Table, TableAlignment, TableRow, TableSortDirection } from './index';
 import { Icon } from '../../';
 
 export default {
@@ -135,6 +135,36 @@ export const Collapsible = (): React.ReactElement => {
       rows={rows}
       selectedRowIds={selectedRowIds}
       onRowClick={onRowClick}
+    />
+  );
+};
+
+export const FixedHeader = (): React.ReactElement => {
+  const manyRows: TableRow[] = Array.from(new Array<string>(100).keys()).map(
+    (val, idx) => ({
+      id: idx.toString(),
+      collapsibleContent: <div>content {idx}</div>,
+      cells: [
+        {
+          content: <Icon type="addressBook" size="sm" />,
+        },
+        {
+          content: idx.toString(),
+          alignment: TableAlignment.right,
+        },
+        {
+          content: `Row #${idx}`,
+          alignment: TableAlignment.right,
+        },
+      ],
+    })
+  );
+  return (
+    <Table
+      isStickyHeader={true}
+      maxHeight={300}
+      headers={headerCells}
+      rows={manyRows}
     />
   );
 };
