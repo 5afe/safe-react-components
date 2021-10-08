b5f5ea03efbec2e4434fb27b9a8b89998361d6fa
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2021-10-08 10:09:49 +0200

    Enhance the TextField component (#156)
    * Use the .attrs constructor to pass additional props to the component
    
    * Pass color prop as a const
    
    * Refactor of TextField types
    
    Use of conditional props to receive an input and use its own onChange instead of a onChange handler passed in the props.
    
    Co-authored-by: Aaron Cook <iamacook@users.noreply.github.com>
    
    * Standardise CustomTextField props
    
    Co-authored-by: DiogoSoaress <DiogoSoaress@users.noreply.github.com>
    
    * Recreate snapshots
    
    * Fix readOnly prop always true
    
    * Update tests snapshots
    
    Co-authored-by: Aaron Cook <iamacook@users.noreply.github.com>
    Co-authored-by: iamacook <aaron.cook@gnosis.pm>
    Co-authored-by: DiogoSoaress <DiogoSoaress@users.noreply.github.com>

--- src/inputs/TextField/index.tsx
+++ src/inputs/TextField/index.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import TextFieldMui, { TextFieldProps } from '@material-ui/core/TextField';
-import InputAdornment from '@material-ui/core/InputAdornment';
+import TextFieldMui, {
+  StandardTextFieldProps,
+  TextFieldProps,
+} from '@material-ui/core/TextField';
+import { InputBaseProps, InputAdornment } from '@material-ui/core';
 import styled from 'styled-components';
 
-type Props = {
-  value: string;
-  label: string;
+type Props = Omit<TextFieldProps, 'InputProps' | 'variant' | 'error'> & {
   readOnly?: boolean;
+  startAdornment?: React.ReactElement;
+  endAdornment?: React.ReactElement;
+  input?: Omit<StandardTextFieldProps['inputProps'], 'onChange'>;
+  // Final Form 'FieldProps' types: https://final-form.org/docs/react-final-form/types/FieldProps
   meta?: {
     error?: string;
   };
-  input?: React.InputHTMLAttributes<HTMLInputElement>; // added for compatibility with react-final-form
-  startAdornment?: React.ReactElement;
-  endAdornment?: React.ReactElement;
-  className?: string;
-} & React.InputHTMLAttributes<HTMLInputElement>;
+};
 
 const CustomTextField = styled((props: TextFieldProps) => (
   <TextFieldMui {...props} />
@@ -31,73 +32,44 @@ const CustomTextField = styled((props: TextFieldProps) => (
     }
 
     .MuiFormLabel-root.Mui-focused {
-      color: ${({ theme, error }) =>
-        error ? theme.colors.error : theme.colors.primary};
+      color: ${({ theme, meta }) =>
+        meta?.error ? theme.colors.error : theme.colors.primary};
     }
 
     .MuiFilledInput-underline:after {
       border-bottom: 2px solid
-        ${({ theme, error }) =>
-          error ? theme.colors.error : theme.colors.primary};
+        ${({ theme, meta }) =>
+          meta?.error ? theme.colors.error : theme.colors.primary};
     }
   }
 `;
 
 function TextField({
-  input,
-  value,
-  onChange,
+  input: inputProps,
   meta,
-  readOnly,
   label,
+  readOnly,
   startAdornment,
   endAdornment,
-  className,
   ...rest
 }: Props): React.ReactElement {
-  const customProps = {
-    error: meta && !!meta.error,
-    label: (meta && meta.error) || label,
-    variant: 'filled' as const,
-    InputProps: {
-      readOnly,
-      startAdornment: startAdornment ? (
-        <InputAdornment position="start">{startAdornment}</InputAdornment>
-      ) : null,
-      endAdornment: endAdornment ? (
-        <InputAdornment position="end">{endAdornment}</InputAdornment>
-      ) : null,
-    },
-    disabled: readOnly,
-    readOnly: readOnly,
+  const customInputBaseProps: InputBaseProps = {
+    startAdornment: startAdornment ? (
+      <InputAdornment position="start">{startAdornment}</InputAdornment>
+    ) : null,
+    endAdornment: endAdornment ? (
+      <InputAdornment position="end">{endAdornment}</InputAdornment>
+    ) : null,
+    readOnly,
   };
-
-  if (input) {
-    const { name, value, ...inputRest } = input;
-    return (
-      <CustomTextField
-        {...rest}
-        {...customProps}
-        inputProps={inputRest}
-        className={className}
-        size={undefined}
-        name={name}
-        checked={!!value}
-        color="primary"
-        value={value as string}
-      />
-    );
-  }
-
   return (
     <CustomTextField
       {...rest}
-      size={undefined}
-      {...customProps}
-      className={className}
-      value={value}
+      error={!!meta?.error}
+      label={meta?.error || label}
+      InputProps={customInputBaseProps}
+      inputProps={inputProps}
       color="primary"
-      onChange={onChange}
     />
   );
 }
