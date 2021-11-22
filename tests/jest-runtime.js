9f8f7f4190696a23ea111545326a7ff270d3b0c5
Daniel Somoza <daniel.somoza@gnosis.pm>
2021-11-22 13:11:44 +0100

    Added Fixed Number Format in tests

--- tests/jest-runtime.js
+++ tests/jest-runtime.js
@@ -3,3 +3,13 @@ import 'jest-styled-components'; // makes classnames deterministic
 jest.mock('@material-ui/core/Portal', () => {
   return ({ children }) => children;
 });
+
+// to avoid failing tests in some environments
+const NumberFormat = Intl.NumberFormat;
+const englishTestLocale = 'en';
+
+jest
+  .spyOn(Intl, 'NumberFormat')
+  .mockImplementation(
+    (locale, ...rest) => new NumberFormat([englishTestLocale], ...rest)
+  );
