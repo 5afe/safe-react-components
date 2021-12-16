48d8de004ed2656ae72a3156224d881af2e7be68
Yago Pérez Vázquez <yagoperez@outlook.com>
2021-12-16 13:24:33 +0100

    Improve test data generation

--- src/dataDisplay/DataTable/dataTable.stories.tsx
+++ src/dataDisplay/DataTable/dataTable.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { GridDensityTypes } from '@mui/x-data-grid';
-
+import { GridDensityTypes, GridRowsProp } from '@mui/x-data-grid';
 import DataTable from './index';
 
 export default {
@@ -37,24 +36,26 @@ const columns = [
   },
 ];
 
-const rows = [
-  { id: '1', asset: 'Token 1', amount: '3.5', value: '$10,000.35' },
-  { id: '2', asset: 'Token 2', amount: '100', value: '$400,000.23' },
-  { id: '3', asset: 'Token 3', amount: '32', value: '$1,000.54' },
-  { id: '4', asset: 'Token 4', amount: '3.5', value: '$10,000.35' },
-  { id: '5', asset: 'Token 5', amount: '100', value: '$400,000.23' },
-  { id: '6', asset: 'Token 6', amount: '32', value: '$1,000.54' },
-  { id: '7', asset: 'Token 7', amount: '3.5', value: '$10,000.35' },
-  { id: '8', asset: 'Token 8', amount: '100', value: '$400,000.23' },
-  { id: '9', asset: 'Token 9', amount: '32', value: '$1,000.54' },
-  { id: '10', asset: 'Token 10', amount: '3.5', value: '$10,000.35' },
-  { id: '11', asset: 'Token 11', amount: '100', value: '$400,000.23' },
-  { id: '12', asset: 'Token 12', amount: '32', value: '$1,000.54' },
-  { id: '13', asset: 'Token 13', amount: '3.5', value: '$10,000.35' },
-  { id: '14', asset: 'Token 14', amount: '100', value: '$400,000.23' },
-  { id: '15', asset: 'Token 15', amount: '32', value: '$1,000.54' },
-  { id: '16', asset: 'Token 16', amount: '3.5', value: '$10,000.35' },
-];
+const randomIntFromInterval = (min: number, max: number): number => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const generateTestData = (howMany: number): GridRowsProp => {
+  const testData = [];
+
+  for (let index = 0; index < howMany; index++) {
+    testData.push({
+      id: index,
+      asset: `Token ${index}`,
+      amount: randomIntFromInterval(1, 100),
+      value: `$${randomIntFromInterval(0, 10000)}`,
+    });
+  }
+
+  return testData;
+};
+
+const rows = generateTestData(20);
 
 export const DataGrid = (): React.ReactElement => {
   return (
