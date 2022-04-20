4e8d11ab43e250b2a1d83ea8f69dff4d5b308388
Daniel Sanchez <daniel.sanchez@gnosis.pm>
2022-04-20 16:00:30 +0200

    Remove underline from links

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,9 +2,9 @@ module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: [
     '@storybook/addon-actions',
-    '@storybook/addon-links',
     '@storybook/addon-docs',
+    '@storybook/addon-controls',
+    '@storybook/addon-links',
     'storybook-addon-react-docgen',
-    'storybook-addon-controls',
   ],
 };
