16e802fb3cd1796091cd5e1638f3911d96e4089a
Usame Algan <usame.algan@gnosis.pm>
2022-05-20 14:12:01 +0200

    style: Adjust typography variants

--- src/revamp/theme/safeTheme.tsx
+++ src/revamp/theme/safeTheme.tsx
@@ -25,6 +25,41 @@ const safeThemeOptions: ThemeOptions = {
 
   typography: {
     fontFamily: `'Averta', 'Roboto', 'Helvetica Neue', 'Arial', 'Segoe UI', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', '-apple-system', 'BlinkMacSystemFont', sans-serif`,
+    h1: {
+      fontSize: 32,
+      lineHeight: '36px',
+      fontWeight: 'bold',
+    },
+    h2: {
+      fontSize: 27,
+      lineHeight: '34px',
+      fontWeight: 'bold',
+    },
+    h3: {
+      fontSize: 24,
+      lineHeight: '30px',
+    },
+    h4: {
+      fontSize: 20,
+      lineHeight: '26px',
+    },
+    body1: {
+      fontSize: 16,
+      lineHeight: '22px',
+    },
+    body2: {
+      fontSize: 14,
+      lineHeight: '20px',
+    },
+    caption: {
+      fontSize: 12,
+      lineHeight: '16px',
+    },
+    overline: {
+      fontSize: 11,
+      lineHeight: '14px',
+      textTransform: 'uppercase',
+    },
   },
 
   components: {
