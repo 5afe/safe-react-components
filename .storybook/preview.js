26a30ef1fe4fa013d0900eeab83822c732e3d952
Yago Pérez Vázquez <yago@safe.global>
2023-01-20 18:20:31 +0100

    Add global theme mode change

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,14 +2,33 @@ import React from 'react';
 import { addDecorator } from '@storybook/react';
 import { addParameters } from '@storybook/react';
 
-import createSafeTheme from '../src/theme/safeTheme.tsx';
-import { ThemeProvider } from '@mui/material';
+import { Stack, ThemeProvider, Typography, IconButton } from '@mui/material';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 
-addDecorator((storyFn) => (
-  <>
-    <ThemeProvider theme={createSafeTheme('light')}>{storyFn()}</ThemeProvider>
-  </>
-));
+import createSafeTheme from '../src/theme/safeTheme.tsx';
+import useThemeMode from '../src/hooks/useThemeMode';
+
+addDecorator((storyFn) => {
+  const { switchThemeMode, themeMode } = useThemeMode();
+
+  return (
+    <ThemeProvider theme={createSafeTheme(themeMode)}>
+      <Stack spacing={2} direction="row" alignItems="center">
+        {/* dark mode switch */}
+        <Typography>{themeMode} mode</Typography>
+        <IconButton onClick={switchThemeMode} color="inherit">
+          {themeMode === 'light' ? (
+            <Brightness4Icon /> // light theme mode icon
+          ) : (
+            <Brightness7Icon /> // dark theme mode icon
+          )}
+        </IconButton>
+      </Stack>
+      {storyFn()}
+    </ThemeProvider>
+  );
+});
 
 addParameters({
   options: {
--- src/demo/Demo.stories.tsx
+++ src/demo/Demo.stories.tsx
@@ -5,7 +5,6 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Tooltip from '@mui/material/Tooltip';
-import IconButton from '@mui/material/IconButton';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import FormControl from '@mui/material/FormControl';
@@ -13,124 +12,99 @@ import InputLabel from '@mui/material/InputLabel';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-
-import useThemeMode from '../hooks/useThemeMode';
-
 export const SimpleDemo = (): React.ReactElement => {
-  const {
-    theme: safeThemeWithMode,
-    switchThemeMode,
-    themeMode,
-  } = useThemeMode();
-
   return (
-    <ThemeProvider theme={safeThemeWithMode}>
-      <Card>
-        <CardContent>
-          <Stack direction="column" spacing={4}>
-            <Stack spacing={2} direction="row" alignItems="center">
-              {/* dark mode switch */}
-              <Typography>{themeMode} mode</Typography>
-              <IconButton onClick={switchThemeMode} color="inherit">
-                {themeMode === 'light' ? (
-                  <Brightness4Icon /> // light theme mode icon
-                ) : (
-                  <Brightness7Icon /> // dark theme mode icon
-                )}
-              </IconButton>
-            </Stack>
+    <Card>
+      <CardContent>
+        <Stack direction="column" spacing={4}>
+          <div>
+            {/* Typography demo */}
+            <Typography variant="h1">H1 Title</Typography>
+            <Typography variant="h2">H2 Title</Typography>
+            <Typography variant="h3">H3 Title</Typography>
+            <Typography variant="h4">H4 Title</Typography>
+            <Typography variant="h5">H5 Title</Typography>
+            <Typography variant="h6">H6 Title</Typography>
+            <Typography variant="body1">Body 1</Typography>
+            <Typography variant="body2">Body 2</Typography>
+            <Typography variant="caption">caption</Typography>
+            <Typography variant="overline">overline</Typography>
+            <Typography>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatum cum expedita, officiis fugiat et veritatis recusandae
+              voluptate eligendi tempora laudantium. Molestias aliquam eveniet
+              eum iure rem exercitationem quibusdam eos quae!
+            </Typography>
+          </div>
 
-            <div>
-              {/* Typography demo */}
-              <Typography variant="h1">H1 Title</Typography>
-              <Typography variant="h2">H2 Title</Typography>
-              <Typography variant="h3">H3 Title</Typography>
-              <Typography variant="h4">H4 Title</Typography>
-              <Typography variant="h5">H5 Title</Typography>
-              <Typography variant="h6">H6 Title</Typography>
-              <Typography variant="body1">Body 1</Typography>
-              <Typography variant="body2">Body 2</Typography>
-              <Typography variant="caption">caption</Typography>
-              <Typography variant="overline">overline</Typography>
-              <Typography>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatum cum expedita, officiis fugiat et veritatis recusandae
-                voluptate eligendi tempora laudantium. Molestias aliquam eveniet
-                eum iure rem exercitationem quibusdam eos quae!
-              </Typography>
-            </div>
-
-            <Stack spacing={2} direction="column" alignItems={'flex-start'}>
-              {/* TextField demo */}
-              <Typography variant="h5">TextField</Typography>
-              <TextField
-                id="outlined-basic-theme-mode"
-                label={`Textfield in ${themeMode} mode`}
-                variant="outlined"
-              />
-            </Stack>
-
-            {/* Buttons demo */}
-            <Typography variant="h5">Buttons</Typography>
-            <Stack spacing={2} direction="row">
-              <Button>Text</Button>
-              <Button variant="contained">Contained</Button>
-              <Button variant="outlined">Outlined</Button>
-            </Stack>
-
-            <Stack spacing={2} direction="row">
-              <Button variant="text" color="secondary">
-                Text
-              </Button>
-              <Button variant="contained" color="secondary">
-                Contained
-              </Button>
-              <Button variant="outlined" color="secondary">
-                Outlined
-              </Button>
-            </Stack>
-
-            <Stack spacing={2} direction="row">
-              <Button variant="text" color="error">
-                Delete
-              </Button>
-              <Button variant="contained" color="error">
-                Delete
-              </Button>
-              <Button variant="outlined" color="error">
-                Delete
-              </Button>
-            </Stack>
-
-            {/* Select demo */}
-            <Typography variant="h5">Select</Typography>
-            <Stack spacing={2} direction="row" maxWidth={150}>
-              <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Currency</InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  label="Currency"
-                  defaultValue="USD">
-                  <MenuItem value="USD">USD</MenuItem>
-                  <MenuItem value="EUR">EUR</MenuItem>
-                  <MenuItem value="ARS">ARS</MenuItem>
-                </Select>
-              </FormControl>
-            </Stack>
-
-            <Stack spacing={2} direction="column" alignItems={'flex-start'}>
-              <Typography variant="h5">Tooltip</Typography>
-              <Tooltip title="Tooltip! :D" arrow placement="top">
-                <Button>Hover!</Button>
-              </Tooltip>
-            </Stack>
+          <Stack spacing={2} direction="column" alignItems={'flex-start'}>
+            {/* TextField demo */}
+            <Typography variant="h5">TextField</Typography>
+            <TextField
+              id="outlined-basic-theme-mode"
+              label="Textfield"
+              variant="outlined"
+            />
           </Stack>
-        </CardContent>
-      </Card>
-    </ThemeProvider>
+
+          {/* Buttons demo */}
+          <Typography variant="h5">Buttons</Typography>
+          <Stack spacing={2} direction="row">
+            <Button>Text</Button>
+            <Button variant="contained">Contained</Button>
+            <Button variant="outlined">Outlined</Button>
+          </Stack>
+
+          <Stack spacing={2} direction="row">
+            <Button variant="text" color="secondary">
+              Text
+            </Button>
+            <Button variant="contained" color="secondary">
+              Contained
+            </Button>
+            <Button variant="outlined" color="secondary">
+              Outlined
+            </Button>
+          </Stack>
+
+          <Stack spacing={2} direction="row">
+            <Button variant="text" color="error">
+              Delete
+            </Button>
+            <Button variant="contained" color="error">
+              Delete
+            </Button>
+            <Button variant="outlined" color="error">
+              Delete
+            </Button>
+          </Stack>
+
+          {/* Select demo */}
+          <Typography variant="h5">Select</Typography>
+          <Stack spacing={2} direction="row" maxWidth={150}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Currency</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                label="Currency"
+                defaultValue="USD">
+                <MenuItem value="USD">USD</MenuItem>
+                <MenuItem value="EUR">EUR</MenuItem>
+                <MenuItem value="ARS">ARS</MenuItem>
+              </Select>
+            </FormControl>
+          </Stack>
+
+          <Stack spacing={2} direction="column" alignItems={'flex-start'}>
+            <Typography variant="h5">Tooltip</Typography>
+            <Tooltip title="Tooltip! :D" arrow placement="top">
+              <Button>Hover!</Button>
+            </Tooltip>
+          </Stack>
+        </Stack>
+      </CardContent>
+    </Card>
   );
 };
 
