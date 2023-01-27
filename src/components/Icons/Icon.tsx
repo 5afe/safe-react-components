c798a82a3a06fcdc1c11394f1dac773f110cc828
Yago Pérez Vázquez <yago@safe.global>
2023-01-27 22:50:34 +0100

    use themes from sx as now it works

--- src/components/Icons/Icon.tsx
+++ src/components/Icons/Icon.tsx
@@ -1,26 +1,23 @@
 import * as React from 'react';
 import SvgIcon from '@mui/material/SvgIcon';
-import usePalette from '../../hooks/usePalette';
 
 type IconProps = {
   component: typeof SvgIcon;
 };
 
 const Icon = ({ component }: IconProps): React.ReactElement => {
-  const palette = usePalette();
-
   return (
     <SvgIcon
       component={component}
       inheritViewBox
       fontSize="inherit"
-      sx={{
+      sx={(theme) => ({
         width: '1rem',
         height: '1rem',
         '& path': {
-          fill: palette.border.main,
+          fill: theme.palette.border.main,
         },
-      }}
+      })}
     />
   );
 };
