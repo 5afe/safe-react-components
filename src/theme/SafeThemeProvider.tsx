4645eb154468d90cdeee9984c7395fb35dc4dcfd
Yago Pérez Vázquez <yago@safe.global>
2023-01-30 10:42:30 +0100

    add comment

--- src/theme/SafeThemeProvider.tsx
+++ src/theme/SafeThemeProvider.tsx
@@ -2,6 +2,11 @@ import * as React from 'react';
 import { PaletteMode, Theme, ThemeProvider } from '@mui/material';
 import createSafeTheme from './safeTheme';
 
+// This component is necessary to make the theme available in the library components
+// Is not enough wrapping the client app with the regular ThemeProvider as the library components
+// are not aware of the theme context:
+// https://github.com/mui/material-ui/issues/32806
+// https://stackoverflow.com/questions/69072004/material-ui-theme-not-working-in-shared-component
 type SafeThemeProviderProps = {
   children: (theme: Theme) => React.ReactNode;
   mode: PaletteMode;
